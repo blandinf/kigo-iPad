be7c33de13252eec8fe047f6cddc57915033ff17
Florian Blandin <pro.blandinf@gmail.com>
2020-05-10 21:08:21 +0200

    ui view extension for fullscreen

--- Kigo/extensions/viewWholeScreen.swift
+++ Kigo/extensions/viewWholeScreen.swift
@@ -6,4 +6,13 @@
 //  Copyright © 2020 blandinf. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+extension UIView {
+    func pinEdges(to other: UIView) {
+        leadingAnchor.constraint(equalTo: other.leadingAnchor).isActive = true
+        trailingAnchor.constraint(equalTo: other.trailingAnchor).isActive = true
+        topAnchor.constraint(equalTo: other.topAnchor).isActive = true
+        bottomAnchor.constraint(equalTo: other.bottomAnchor).isActive = true
+    }
+}
